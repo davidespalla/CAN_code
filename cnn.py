0a893d66d0be0174412484d148681cc8839652a0
Davide <spalladavide.92@gmail.com>
2019-02-08 17:17:42 +0100

    Initial commit

---
+++ cnn.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Network:
+    def __init__(self,N,p):
+        self.N=N
+        self.p=p
+        self.J=np.zeros((N,N))
+        
+    def CreateJ(self):
+        
+    def EvolveDynamics(self,a1,a2):
+        
+    def TrackDynamics(self,a1,a2):
+        
